c69121248f047b2747245483a25989025cd2f722
joshuanatan <joshuanatan.jn@gmail.com>
2019-02-20 02:18:51 +0700

    update format controller

--- application/controllers/format_controller.php
+++ application/controllers/format_controller.php
@@ -1,87 +1,44 @@
 <?php
 defined("BASEPATH") OR exit("No Direct Script");
 
-class format_model extends CI_Controller{
+class format_controller extends CI_Controller{
     public function __construct(){
         parent::__construct();
-        $this->load->model(array(""));
+        $this->req();
+        //$this->load->model(array("Mdkelas"));
+    }
+    public function req(){
+        $this->load->view("req/html-open");
+        $this->load->view("req/head");
+        $this->load->view("req/menu");
+        $this->load->view("req/content-container-open");
+        $this->load->view("req/header-open");
+        $this->load->view("req/logo");
+        $this->load->view("req/header-widget-open");
+        $this->load->view("req/search");
+        $this->load->view("req/message");
+        $this->load->view("req/notifikasi");
+        $this->load->view("req/header-widget-close");
+        $this->load->view("req/profile");
+        $this->load->view("req/header-close");
+    }
+    public function close(){
+        
+        $this->load->view("req/footer");
+        $this->load->view("req/content-container-close");
+        $this->load->view("req/html-close");
+        $this->load->view("script/js-main");
     }
     public function index(){
-        if($this->session->role == "admin"){
-            $this->load->view("");
-        }
-        else{
-            echo '<script language="javascript">';
-            echo 'alert("message successfully sent")';
-            echo '</script>';
-            redirect("user/signin");
-        }
-    }
-    public function insert(){
+        //$this->load->view("namapage/breadcrumb");
+        $this->load->view("req/open-content");
+        /* disini custom contentnya pake apapun yang dibutuhkan */
         
-        if($this->session->role == "admin"){
-            $this->load->view("");
-        }
-        else{
-            echo '<script language="javascript">';
-            echo 'alert("message successfully sent")';
-            echo '</script>';
-            redirect("user/signin");
-        }
-    }
-    public function submit(){
-        
-        if($this->session->role == "admin"){
-            redirect("");        
-        }
-        else{
-            echo '<script language="javascript">';
-            echo 'alert("message successfully sent")';
-            echo '</script>';
-            redirect("user/signin");
-        }
-    }
-    public function edit($i){
-        if($this->session->role == "admin"){
-            $where = array(
-                "id" => $i
-            );
-            $data = array(
-            
-            );
-            $this->load->view("",$data);
-        }
-        else{
-            echo '<script language="javascript">';
-            echo 'alert("message successfully sent")';
-            echo '</script>';
-            redirect("user/signin");
-        }
-    }
-    public function update(){
-        
-        if($this->session->role == "admin"){
-            redirect("");        
-        }
-        else{
-            echo '<script language="javascript">';
-            echo 'alert("message successfully sent")';
-            echo '</script>';
-            redirect("user/signin");
-        }
-    }
-    public function delete($i){
-        if($this->session->role == "admin"){
-            $data = array(
-                "status" => 1
-            );
-            redirect("");
-        }
-        else{
-            echo '<script language="javascript">';
-            echo 'alert("message successfully sent")';
-            echo '</script>';
-            redirect("user/signin");
-        }
+        /* endnya disini */
+        $this->load->view("req/close-content");
+        $this->load->view("req/space");
+        $this->close();
+        $this->load->view("script/js-calender");
+        $this->load->view("script/js-datatable");
     }
 }
